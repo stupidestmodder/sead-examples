a7648286c354ea896efdfb5802192432d6af60f8
STUPID Modder <stupidmoddersoup@gmail.com>
2026-05-22 23:24:03 -0300

    Update sead

--- premake5.lua
+++ premake5.lua
@@ -99,6 +99,14 @@ project "sead-examples"
 
         -- TODO: Remove
         defines { "SEAD_PLATFORM_POSIX", "SEAD_PLATFORM_MACOSX" }
+        links {
+            "Cocoa.framework",
+            "IOKit.framework",
+            "CoreVideo.framework",
+            "OpenGL.framework",
+            "QuartzCore.framework",
+            "UniformTypeIdentifiers.framework"
+        }
 
     filter "platforms:GLFW_*"
         defines {
